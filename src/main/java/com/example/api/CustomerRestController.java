09537849c69b1d9b1e08ef97e015ef535870a5e7
nishina <n.toshi.1.014@gmail.com>
2016-09-24 00:37:07 +0900

    add paging logic

--- src/main/java/com/example/api/CustomerRestController.java
+++ src/main/java/com/example/api/CustomerRestController.java
@@ -3,10 +3,11 @@ package com.example.api;
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /**
  * Created by nishina on 2016/09/23.
@@ -19,10 +20,16 @@ public class CustomerRestController {
     CustomerService customerService;
 
     @GetMapping
-    List<Customer> getCustomers() {
-        List<Customer> customers = customerService.findAll();
+    Page<Customer> getCustomers(@PageableDefault Pageable pageable) {
+        Page<Customer> customers = customerService.findAll(pageable);
         return customers;
     }
+
+//    @GetMapping
+//    List<Customer> getCustomers() {
+//        List<Customer> customers = customerService.findAll();
+//        return customers;
+//    }
 
     @GetMapping(path = "{id}")
     Customer getCustomer(@PathVariable Integer id) {
@@ -30,13 +37,23 @@ public class CustomerRestController {
         return customer;
     }
 
-    @PostMapping(path = "{id}")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     Customer postCustomers(@RequestBody Customer customer) {
         return customerService.create(customer);
     }
 
-    
+    @PutMapping(path = "{id}")
+    Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        customer.setId(id);
+        return customerService.update(customer);
+    }
+
+    @DeleteMapping(path = "{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteCustomer(@PathVariable Integer id) {
+        customerService.delete(id);
+    }
 
 
 }
